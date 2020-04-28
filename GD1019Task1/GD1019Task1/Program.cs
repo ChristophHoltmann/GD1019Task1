a353a33766256dce8b94bde3d6f3962354059bf1
Christoph Holtmann <christoph@holtmann.world>
2020-04-28 11:26:02 +0200

    Initialize project

---
+++ GD1019Task1/GD1019Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GD1019Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
